67c9a8df68a373bbdea48ef714beae7e1d700f5f
Jethro Kuan <jethrokuan95@gmail.com>
2012-10-18 14:47:34 +0800

    Fixed reveal! :D

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,7 +2,7 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-      can :manage, :all if user.role == "member"
+      can :manage, :all if user.role == "staff"
       can :read, :all if user.role =="user"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-	ROLES = %w[member user]
+	ROLES = %w[staff user]
 
   attr_accessible :email, :password, :password_confirmation, :remember_me, :role, :school_year, :school, :surname, :givenname, :checkpoint_id, :subject_ids
 end
