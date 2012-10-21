b3cd48a2b93d4b46b2cbfaac0ee69a3024ea97a1
Jethro Kuan <jethrokuan95@gmail.com>
2012-10-21 14:21:20 +0800

    Added 404 Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
+
 	rescue_from CanCan::AccessDenied do |exception|
 		flash[:alert] = "Access denied. You are not logged in as a member of openlectures, or you are not a member of openlectures."
 		redirect_to root_url
--- app/controllers/usr_controller.rb
+++ app/controllers/usr_controller.rb
@@ -1,5 +1,9 @@
 class UsrController < ApplicationController
   def manage
+  	if staff_signed_in?
+  		redirect_to root_path, alert: "You are unauthorized to access this action."
+  	else
+  	end
   end
 
   def profile
