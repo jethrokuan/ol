e8e0c4bad7ef8b00f7e44cfef5a948dfb8389ab6
Kenneth Lim <kenlimmj@gmail.com>
2012-11-06 22:03:34 +0800

    Bootstrap Typeahead
    Added the html for the typeahead autocomplete. Still needs to parse the json in order to work.
    
    Signed-off-by: Kenneth Lim <kenlimmj@gmail.com>

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,5 @@
 //= require twitter/bootstrap/bootstrap-popover
 //= require twitter/bootstrap/bootstrap-transition
 //= require twitter/bootstrap/bootstrap-affix
+//= require twitter/bootstrap/bootstrap-typeahead
 //= require iosfix
