dd459d4b75c10f021e06c8e6a3249f8aa86fc1fd
Jethro Kuan <jethrokuan95@gmail.com>
2012-11-12 18:44:17 +0800

    added staff avatar (which would soon be removed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112102634) do
+ActiveRecord::Schema.define(:version => 20121112103148) do
 
   create_table "checkpoints", :force => true do |t|
     t.string   "checkpoint"
@@ -66,6 +66,10 @@ ActiveRecord::Schema.define(:version => 20121112102634) do
     t.string   "givenname"
     t.string   "surname"
     t.string   "school"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "staffs", ["email"], :name => "index_staffs_on_email", :unique => true
