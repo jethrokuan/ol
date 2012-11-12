a1e2dad445681e2ae4326f6bae5006d0442155db
Kenneth Lim <kenlimmj@gmail.com>
2012-11-12 17:31:19 +0800

    Whole bunch of format updates (See Description)
    Let's see…
    
    Trimmed utilities.css for useless classes
    
    Linked the login template to the same partial so the formatting is consistent.
    
    Devise shared links goes to hell as a consequence
    
    Simpleforms goes to hell because it tries to act smart and fails
    
    Added the school autocomplete to the staff sign up form
    
    Moved the reveal style out of the 404 handler and into the reveal template
    
    Abstracted application and reveal layouts for consistency and ease of management.
    
    Moved the bootstrap flash helper and GCF into div.container so it obeys responsiveness
    
    Updated home page to use the same navsubjects partial as the sidebar and deleted all evidence of my incompetency
    
    Removed about partial, which was actually a duplicate of pages partial
    
    Added two nice right-facing carets to the search bar so people get a sense they are actually being productive.
    
    In a glorious show of decisiveness, spliced the get started link back out again
    
    Fixed the incorrect path for the png fallback for the ol logo. Bloody equal signs are going to be the death of me one day.
    
    Moved a lot of the sidebar logic around using partials
    
    Changed input-xxlarge to input-block-level where possible, so we don't rely on an actual fixed width.
    
    In the process of adding a dandy live search function to the staff database. Standby for magic.
    
    Signed-off-by: Kenneth Lim <kenlimmj@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -48,18 +48,6 @@ ActiveRecord::Schema.define(:version => 20121108023455) do
     t.datetime "updated_at",    :null => false
   end
 
-  create_table "schedules", :force => true do |t|
-    t.string   "checkpoint"
-    t.string   "lesson"
-    t.string   "subject"
-    t.string   "slug"
-    t.date     "date"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "schedules", ["slug"], :name => "index_schedules_on_slug", :unique => true
-
   create_table "staffs", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
